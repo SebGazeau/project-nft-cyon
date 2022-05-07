a031eda2b977b2cbc1f737cf97eac3d8f5670477
AlexisJsbingithub <alexis.mendoza.perso@gmail.com>
2022-05-07 10:47:36 +0200

    Contracts NFTCollection et Factory

--- contracts/NFTCollectionFactory.sol
+++ contracts/NFTCollectionFactory.sol
@@ -3,11 +3,6 @@ pragma solidity 0.8.13;
 import "./NFTs_Collections.sol";
 
 contract NFTCollectionFactory {
-    //Création d'une collection :
-    //Nom de la collection et symbole (a minima) + création via assembly + event de la création
-    //Création d'un NFT :
-    //Nom du NFT et symbole (a minima) + création via assembly + event de la création
-
     event NFTCollectionCreated(
         string _NFTName,
         address _collectionAddress,
@@ -16,9 +11,7 @@ contract NFTCollectionFactory {
 
     function createNFTCollection(
         string memory _NFTName,
-        string memory _NFTDescription,
-        uint256 _price,
-        bool _favorite
+        string memory _NFTSymbole
     ) external returns (address collectionAddress) {
         // Import the bytecode of the contract to deploy
         bytes memory collectionBytecode = type(NFTs_Collections).creationCode;
@@ -38,13 +31,7 @@ contract NFTCollectionFactory {
             }
         }
         // Initialize the collection contract with the artist settings
-        NFTs_Collections(collectionAddress).init(
-            msg.sender,
-            _NFTName,
-            _NFTDescription,
-            _price,
-            _favorite
-        );
+        NFTs_Collections(collectionAddress).initialize(_NFTName, _NFTSymbole);
 
         emit NFTCollectionCreated(_NFTName, collectionAddress, block.timestamp);
     }
--- contracts/NFTs_Collections.sol
+++ contracts/NFTs_Collections.sol
@@ -2,51 +2,51 @@
 pragma solidity 0.8.13;
 
 import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+//import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+//import "../node_modules/@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "../node_modules/@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 
-contract NFTs_Collections is ERC721URIStorage {
+contract NFTs_Collections is Initializable, ERC721URIStorageUpgradeable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    struct Collections {
+    struct NFT {
         string name;
         string description;
+        string tag;
         uint256 price;
         bool favorite;
+        bool isAuctionable;
     }
-    Collections[] collections;
+    NFT[] collection;
 
-    constructor() ERC721("Collections", "CL") {}
+    function initialize(string memory _name, string memory _symbol)
+        public
+        initializer
+    {
+        __ERC721_init(_name, _symbol);
+    }
 
-    function MintCollections(
+    function MintNFTCollection(
         address _user,
         string memory _tokenURI,
         string memory _name,
         string memory _description,
+        string memory _tag,
         uint256 _price,
-        bool _favorite
+        bool _favorite,
+        bool _isAuctionable
     ) public returns (uint256) {
         _tokenIds.increment();
-        collections.push(Collections(_name, _description, _price, _favorite));
+        collection.push(
+            NFT(_name, _description, _tag, _price, _favorite, _isAuctionable)
+        );
         uint256 newItemId = _tokenIds.current();
         _mint(_user, newItemId);
         _setTokenURI(newItemId, _tokenURI);
 
         return newItemId;
     }
-
-    function init(
-        address _address,
-        string memory _name,
-        string memory _description,
-        uint256 _price,
-        bool _favorite
-    ) public {
-        //MintCollections
-    }
-
-    function buy() public {}
-
-    function sell() public {}
 }
