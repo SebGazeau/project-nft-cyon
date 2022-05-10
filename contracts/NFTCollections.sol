85dec516029d5dfb7b945c7ce17c53df9e7bba57
DeltaUpsilonDu <subscr1pt10n+g1t@myfr33dom.net>
2022-05-10 17:11:44 +0200

    Some inheritances removed from th NFT collection contract.

--- contracts/NFTCollections.sol
+++ contracts/NFTCollections.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+//import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
-import "../node_modules/@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+//import "../node_modules/@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "../node_modules/@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+//import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 /// @title NFTCollections contract
 /// @author Sebastien Gazeau, Sébastien Dupertuis et Alexis Mendoza
