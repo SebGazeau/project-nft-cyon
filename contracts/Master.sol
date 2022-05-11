d1f43e54f463f07da2c4b870550ef0b54f34a8f7
DeltaUpsilonDu <subscr1pt10n+g1t@myfr33dom.net>
2022-05-11 11:15:33 +0200

    Adding of the possibility to create a NFT from the master contract.

--- contracts/Master.sol
+++ contracts/Master.sol
@@ -12,8 +12,21 @@ contract Master is Auction, NFTCollectionFactory {
     //------------------------------------------------------------------------------------
     // ------------------------------------Events-----------------------------------------
     //------------------------------------------------------------------------------------
-
+    
     //------------------------------------------------------------------------------------
     // -----------------------------------Functions---------------------------------------
     //------------------------------------------------------------------------------------
+    function createNFT(address _collectionAddress, address _user, string memory _tokenURI, string memory _name, 
+        string memory _description, string memory _tag, address _tokenAddress) external {
+            require(_collectionAddress != address(0),"The collection address needs to be different from zero.");
+            require(_user != address(0),"The user address needs to be different from zero.");  
+
+            NFTCollections(_collectionAddress).MintNFT(_user,_tokenURI,_name,_description,_tag,_tokenAddress,0,false,false);
+        }
+
+
+    //------------------------------------------------------------------------------------
+    // ------------------------------------Getters----------------------------------------
+    //------------------------------------------------------------------------------------
+
 }
