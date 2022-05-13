7a452418b00b3356e14f7e4044f3aff00486e13a
DeltaUpsilonDu <subscr1pt10n+g1t@myfr33dom.net>
2022-05-13 16:44:20 +0200

    Adding of the auction fonctionality.

--- contracts/Master.sol
+++ contracts/Master.sol
@@ -3,12 +3,12 @@ pragma solidity 0.8.13;
 
 import "./Auction.sol";
 import "./NFTCollectionFactory.sol";
-import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /// @title Master contract to manage the decentralized NFT market place 
 /// @author Sebastien Gazeau, Sébastien Dupertuis et Alexis Mendoza
 /// @notice This smart contract is the link between the Dapp and the subcontracts
-contract Master {
+contract Master is Auction {
     
     //------------------------------------------------------------------------------------
     // ----------------------------------Variables----------------------------------------
@@ -63,14 +63,14 @@ contract Master {
     /// @param _price The price to set
     function setNewPrice(address _collectionAddress, uint256 _tokenID, uint256 _price) external {
         require(_collectionAddress != address(0),"The collection address needs to be different from zero.");    // Make sure the address is different from zero
-        require((_tokenID <= NFTCollections(_collectionAddress).getTotalSupply()) && (_tokenID > 0), "This token ID does not exist.");      // Make sure the token ID exists
-        require(NFTCollections(_collectionAddress).ownerOf(_tokenID) == msg.sender,"This NFT does not belong to the current message sender.");  // Make sure the sale comes from the owner
+        //require((_tokenID <= NFTCollections(_collectionAddress).getTotalSupply()) && (_tokenID > 0), "This token ID does not exist.");      // Make sure the token ID exists
+        require(NFTCollections(_collectionAddress).ownerOf(_tokenID) == msg.sender,"This NFT does not belong to the current message sender.");  // Make sure the sale comes from the owner. Revert if the tokenID does not exist (ERC721Upgradeable library)
         require(_price > 0,"Please define a selling price higher than zero.");                                  // Make sure the price is more than zero
 
         NFTCollections(_collectionAddress).setPrice(_tokenID,_price);
     }
 
-    /// @notice This function allows to transfer the money to the NFT owner and the NFT to the buyer.
+    /// @notice This function allows to transfer the money to the NFT owner and the NFT to the buyer after a direct sale.
     /// @dev Call this function when a buyer click on the BUY button
     /// @param _collectionAddress The address of the collection of the NFT to be sold/transfered
     /// @param _tokenID The token ID of the NFT to be sold/transfered
@@ -96,6 +96,62 @@ contract Master {
         emit NFTSold(NFTCollections(_collectionAddress).name(), _collectionAddress, _tokenID, currentOwner, msg.sender, price);
     }
 
+    /// @notice This function allows to initiate an auction by verifying owner and tokenID
+    /// @dev Call this function before starting the auction procedure
+    /// @param _collectionAddress The address of the collection of the NFT to start an auction
+    /// @param _tokenID The token ID of the NFT to start an auction
+    /// @return _validity True if the auction is authorized
+    function requestAuction(address _collectionAddress, uint256 _tokenID) external returns (bool) {
+        require(_collectionAddress != address(0),"The collection address needs to be different from zero.");    // Make sure the address is different from zero
+        require(NFTCollections(_collectionAddress).ownerOf(_tokenID) == msg.sender,"This NFT does not belong to the current message sender.");  // Make sure the sale comes from the owner. Revert if the tokenID does not exist (ERC721Upgradeable library)
+        require(NFTCollections(_collectionAddress).getPrice(_tokenID) == 0,"This NFT is already for sale.");     // Make sure the NFT is not already for sale
+
+        setAuctionValidity(_collectionAddress, _tokenID, msg.sender);
+
+        return true;
+    }
+
+    /// @notice This function allows to close an auction when the time has expired
+    /// @dev Call this function from the front when the auction time has expired
+    /// @param _collectionAddress The address of the collection of the NFT to close the auction
+    /// @param _tokenID The token ID of the NFT to close the auction
+    function closeAuction(address _collectionAddress, uint256 _tokenID) external {
+        require(_collectionAddress != address(0),"The collection address needs to be different from zero.");    // Make sure the address is different from zero
+        NFTCollections(_collectionAddress).ownerOf(_tokenID);       // Revert if the tokenID does not exist (ERC721Upgradeable library)
+        require(this.isInAuction(_collectionAddress,_tokenID),"This NFT did not start an auction.");         // Make sure an auction started
+        require(this.checkAuctionTimeExpired(_collectionAddress,_tokenID), "The auction has not finished yet."); // Make sure the auction is finished
+
+        // Get the data of the auction
+        uint256 price = this.getCurrentHighestBid(_collectionAddress,_tokenID);
+        address currentOwner = NFTCollections(_collectionAddress).ownerOf(_tokenID);
+        address highestBidder = this.getCurrentHighestBidder(_collectionAddress, _tokenID);
+        require(highestBidder != address(0),"Error: the highest bidder is the zero address.");  // Make sure the address of the highest bidder is valid
+
+        // Check if price is more than 0. Otherwise, nobody has bidded
+        if(price > 0) {
+            // Send the ETH to the current NFT owner
+            (bool success, ) = payable(currentOwner).call{value:price}("");
+            require(success, "Failed to send ETH to the NFT owner.");
+
+            // Verify that the highest bidder is not the current owner. Otherwise, nobody has bidded, except the initial bid of the owner (starting price)
+            if(highestBidder != currentOwner) {
+                NFTCollections(_collectionAddress).setPrice(_tokenID,price);        // Update the price of the NFT for event tracking
+            
+                // Send the NFT to the new owner
+                require(NFTCollections(_collectionAddress).getApproved(_tokenID) == address(this),"");       // Make sure the Master contract has the allowance to manage the NFT transfer
+                NFTCollections(_collectionAddress).safeTransferFrom(currentOwner, highestBidder, _tokenID);
+
+                // Reset the price to 0 after the sale for price tracking
+                NFTCollections(_collectionAddress).setPrice(_tokenID,0);
+
+                emit NFTSold(NFTCollections(_collectionAddress).name(), _collectionAddress, _tokenID, currentOwner, highestBidder, price);
+            }
+            
+            // Reset the auction data
+            endAuction(_collectionAddress, _tokenID);
+        }
+    }
+
 
     //------------------------------------------------------------------------------------
     // ------------------------------------Getters----------------------------------------
@@ -106,7 +162,17 @@ contract Master {
     /// @param _tokenID The token ID of the NFT to get the URI
     /// @return _URI The URI data
     function getURI(address _collectionAddress, uint256 _tokenID) external view returns (string memory) {
-        require((_tokenID <= NFTCollections(_collectionAddress).getTotalSupply()) && (_tokenID > 0), "This token ID does not exist.");      // Make sure the token ID exists
+        //require((_tokenID <= NFTCollections(_collectionAddress).getTotalSupply()) && (_tokenID > 0), "This token ID does not exist.");      // Make sure the token ID exists
+        NFTCollections(_collectionAddress).ownerOf(_tokenID);       // Revert if the tokenID does not exist (ERC721Upgradeable library)
         return(NFTCollections(_collectionAddress).tokenURI(_tokenID));
     }
+/*
+    /// @notice This function returns the current owner of the given NFT
+    /// @dev Call this function to get the owner of the NFT. Revert if the tokenID does not exist
+    /// @param _collectionAddress The address of the collection of the NFT to get the owner
+    /// @param _tokenID The token ID of the NFT to get the owner
+    /// @return _address The owner address
+    function getNFTOwner(address _collectionAddress, uint256 _tokenID) external view returns (address) {
+        return(NFTCollections(_collectionAddress).ownerOf(_tokenID));
+    }*/
 }
--- contracts/NFTCollections.sol
+++ contracts/NFTCollections.sol
@@ -141,17 +141,15 @@ contract NFTCollections is ERC721URIStorage {
     /// @param _tokenID The token ID of the NFT to get the price
     /// @return _price The price of the given NFT
     function getPrice(uint256 _tokenID) external view returns (uint256) {
-        require(
-            (_tokenID <= _tokenIds.current()) && (_tokenID > 0),
-            "This token ID does not exist."
-        ); // Make sure the token ID exists
-        return (collection[_tokenID - 1].price);
+        //require((_tokenID <= _tokenIds.current()) && (_tokenID > 0), "This token ID does not exist.");      // Make sure the token ID exists
+        this.ownerOf(_tokenID);       // Revert if the tokenID does not exist (ERC721Upgradeable library)
+        return(collection[_tokenID-1].price);
     }
 
-    /// @notice This function allows to get the total amount of NFTs saved in this contract
+    /*/// @notice This function allows to get the total amount of NFTs saved in this contract
     /// @dev Call this function to check the last created token ID
     /// @return _totalSupply The last created token ID
     function getTotalSupply() external view returns (uint256) {
-        return (_tokenIds.current());
-    }
+        return(_tokenIds.current());
+    }*/
 }
