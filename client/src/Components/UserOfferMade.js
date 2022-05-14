70ed11580df77f9fa112966efe808b0b8b3901f8
AlexisJsbingithub <alexis.mendoza.perso@gmail.com>
2022-05-14 16:36:05 +0200

    Affichage des NFT achetés et vendus finis

--- client/src/Components/UserOfferMade.js
+++ client/src/Components/UserOfferMade.js
@@ -7,17 +7,46 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 export default class UserOfferMade extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+          arrayNFT: [],
+        }
     }
+
+    componentDidMount = async () => {
+      this.Master();
+    }
+    Master = async (e) =>{
+      const options = {
+          fromBlock: 0,              
+          toBlock: 'latest'
+      };
+      if(this.props.state.contractMaster){
+
+        const NFTSolded = await this.props.state.contractMaster.getPastEvents('NFTSold', options);
+
+        if(NFTSolded.length > 0){
+          for(const cc of NFTSolded){       
+            const newOwner = cc.returnValues._newOwner;
+            if(newOwner.toLowerCase() === this.props.state.accounts[0].toLowerCase()){
+              this.setState({
+                arrayNFT: this.state.arrayNFT.concat([{
+                  collectionName: cc.returnValues._collectionName,
+                  price: cc.returnValues._price,
+                  units: cc.returnValues._units
+                }])
+              })
+            }
+          }
+        }
+      }
+    }
+
     render()  {
       return (
         <div>
           <Container className="container-list">
           <ListGroup>
-            <ListGroup.Item>Cras justo odio</ListGroup.Item>
-            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            {(this.state.arrayNFT!=null)?this.state.arrayNFT.map((a) => (<ListGroup.Item>Collection = "{a.collectionName}", Units = "{a.units}", Price = "{a.price}"</ListGroup.Item>)):""}
         </ListGroup>
         </Container>
         </div>
--- client/src/Components/UserOfferReceived.js
+++ client/src/Components/UserOfferReceived.js
@@ -24,34 +24,21 @@ export default class UserOfferReceived extends React.Component {
       if(this.props.state.contractMaster){
 
         const NFTSolded = await this.props.state.contractMaster.getPastEvents('NFTSold', options);
-        console.log("NFTSolded.length=",NFTSolded.length);
 
-        /*event NFTSold(string _collectionName, address _collectionAddress, uint256 _tokenID, address _oldOwner, address _newOwner, uint256 _price, string _units);*/
-
-
-        /*if(NFTSolded.length > 0){
-          for(const cc of NFTSolded){            
-            const owner = await instance.methods.ownerOf(cc.returnValues._tokenID).call();
-            if(owner.toLowerCase() === this.props.state.accounts[0].toLowerCase()){
-              const firstUri = await instance.methods.tokenURI(cc.returnValues._tokenID).call();
+        if(NFTSolded.length > 0){
+          for(const cc of NFTSolded){       
+            const oldOwner = cc.returnValues._oldOwner;
+            if(oldOwner.toLowerCase() === this.props.state.accounts[0].toLowerCase()){
               this.setState({
                 arrayNFT: this.state.arrayNFT.concat([{
-                  firstOwner: cc.returnValues._firstOwner,
-                  creator: cc.returnValues._collectionData._creator, 
-                  tokenURI: cc.returnValues._collectionData._tokenURI, 
-                  name: cc.returnValues._collectionData._name, 
-                  description: cc.returnValues._collectionData._description, 
-                  tag: cc.returnValues._collectionData._tag, 
-                  tokenAddress: cc.returnValues._collectionData._tokenAddress, 
-                  price: cc.returnValues._collectionData._price,
-                  favorite: cc.returnValues._collectionData._favorite, 
-                  url:`https://gateway.pinata.cloud/ipfs/${firstUri}`
+                  collectionName: cc.returnValues._collectionName,
+                  price: cc.returnValues._price,
+                  units: cc.returnValues._units
                 }])
               })
             }
           }
-
-        }*/
+        }
       }
     }
 
@@ -59,21 +46,9 @@ export default class UserOfferReceived extends React.Component {
       return (
         <div>
           <Container className="container-list">
-            {this.state.arrayNFT.map((nft, index) => (
-              <Link key={index} className="link-collection m-2" to={`/collection/nft/${this.address}/${nft.tokenURI}`}>
-                <Card bg={'light'} text={'dark'}>
-                  <Card.Header>{nft.name}</Card.Header>
-                  <Card.Img variant="top" src={nft.tag} />
-                  {/* <Card.Body>
-                    <Card.Title>{variant} Card Title </Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                    </Card.Text>
-                  </Card.Body> */}
-                </Card>
-              </Link>
-            ))}
+          <ListGroup>
+            {(this.state.arrayNFT!=null)?this.state.arrayNFT.map((a) => (<ListGroup.Item>Collection = "{a.collectionName}", Units = "{a.units}", Price = "{a.price}"</ListGroup.Item>)):""}
+        </ListGroup>
         </Container>
         </div>
       );
