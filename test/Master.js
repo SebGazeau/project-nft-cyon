bd1a0a05f56888cacfe0dfd43d5dcf310fcae6e2
DeltaUpsilonDu <subscr1pt10n+g1t@myfr33dom.net>
2022-05-15 23:32:23 +0200

    Adding of tests.

--- test/Master.js
+++ test/Master.js
@@ -6,61 +6,115 @@ const { BN, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
 const {expect} = require('chai');
 
 contract('Master', (accounts) => {
-	const owner = accounts[0];
-	const sendETH = (0.021*10**18);
-	const sendCYON = (0.001*10**18);
+	//------------------------------------------------------------------------------------
+    // ----------------------------------Variables----------------------------------------
+    //------------------------------------------------------------------------------------
+    const owner = accounts[0];
+    const user1 = accounts[1];
+    const user2 = accounts[2];
+    const user3 = accounts[3];
+    const user4 = accounts[4];
+    const addressZero = '0x0000000000000000000000000000000000000000';
+
+	const biddingTime = 60;
+
+    const nftCollectionName = 'Collection1';
+    const nftCollectionSymbol = 'COL1';
+    const tokenURI = 'Test URI';
+    const nftName = 'My first NFT';
+    const nftDescription = 'This is my first NFT';
+    const nftTag = 'Collectible';
+
 	let CYONTokenInstance;
 	let MasterInstance;
-	let FactoryInstance;
 
-	let NFTCollection;
-    let addressCollection;
-	let swapCYONtoETH;
-	describe("Testing create NFT", () => {
-		before(async () => {
+    let masterInstance;
+    let nftFactoryInstance;
+    let nftCollection;
+    let nftCollectionAddress;
+
+	describe("Testing createNFT function", () => {
+		beforeEach(async () => {
 			CYONTokenInstance = await CYONToken.new('10000000000000000000000000',{from:owner});
 			await CYONTokenInstance.transfer(CYONTokenInstance.address, '10000000000000000000000000');
-            MasterInstance = await Master.new(CYONTokenInstance.address,{from:owner});
-            FactoryInstance = await Factory.new({from:owner});
+            masterInstance = await Master.new(CYONTokenInstance.address,{from:owner});
+            nftFactoryInstance = await Factory.new({from:owner});
+			nftCollection = await nftFactoryInstance.createNFTCollection(nftCollectionName,nftCollectionSymbol,{from: user1});
+			nftCollectionAddress = nftCollection.logs[0].args._collectionAddress;
 		});
-		context(('create collection'), () => {
-			it("should create a collection", async () => {
-				NFTCollection = await FactoryInstance.createNFTCollection('Collection Unit Test','CUT',{from: accounts[1]});
-                addressCollection = NFTCollection.logs[0].args._collectionAddress;
-				expect(NFTCollection).to.be.ok;
-			});
-            it("should create NFT", async () => {
-                const NFT = await MasterInstance.createNFT(addressCollection, accounts[1], 'test hash', 'NFT Unit Test','NFT for unit test','NUT', {from: accounts[1]});
-                const NFT2 = await MasterInstance.createNFT(addressCollection, accounts[1], 'test hash', 'NFT Unit Test','NFT for unit test','NUT', {from: accounts[1]});
-                const NFT3 = await MasterInstance.createNFT(addressCollection, accounts[1], 'test hash', 'NFT Unit Test','NFT for unit test','NUT', {from: accounts[1]});
-                console.log()
-                expect(NFTCollection).to.be.ok;
-			});
-            it("should sel a NFT", async () => {
-                const NFTCollectionInstance = await NFTCollection.new
-                const NFT = await MasterInstance.createNFT(addressCollection, accounts[1], 'test hash', 'NFT Unit Test','NFT for unit test','NUT', {from: accounts[1]});
-                expect(NFTCollection).to.be.ok;
-			});
-		})
-		context.skip(('CYON to ETH'), async () => {
-			it('should send ETH balance of contract', async () =>{
-				const balanceCtrEth = await CYONTokenInstance.getBalanceETH({from:owner});
-				expect(balanceCtrEth).to.be.ok;
-			})
-			it("should swap the tokens", async () => {
-				await CYONTokenInstance.approve(CYONTokenInstance.address,new BN(sendCYON.toString()), {from: accounts[1]})
-				swapCYONtoETH = await CYONTokenInstance.swapCYONtoETH(new BN(sendCYON.toString()),{from: accounts[1]});
-				expect(swapCYONtoETH).to.be.ok;
-			});
-			it('get event CYONtoETH', () =>{
-				expectEvent(swapCYONtoETH, 'CYONtoETH', {balanceCYON: new BN(sendCYON.toString()), userAddress: accounts[1]});
-			});
-			it('if the nbr of CYON is greater than or equal to 100, revert',async () =>{
-				await expectRevert(CYONTokenInstance.swapCYONtoETH(new BN(10),{from: accounts[1]}), 'the amount of CYON must be greater than or equal to 100');
-			});
-			it('is greater than the requested transfer, revert ',async () =>{
-				await expectRevert(CYONTokenInstance.swapCYONtoETH(new BN(sendCYON.toString()),{from: accounts[1]}), 'Token 2 allowance too low');
-			});
-		})
+		it("should revert when the collection address is set to zero.", async () => {
+            await expectRevert(masterInstance.createNFT(addressZero, user1, tokenURI, nftName,nftDescription,nftTag, {from: user1}), 'The collection address needs to be different from zero.');
+        });
+		it("should revert when the first owner address is set to zero.", async () => {
+            await expectRevert(masterInstance.createNFT(nftCollectionAddress, addressZero, tokenURI, nftName,nftDescription,nftTag, {from: user1}), 'The user address needs to be different from zero.');
+        });
+		it("should create a NFT.", async () => {
+			await masterInstance.createNFT(nftCollectionAddress, user1, tokenURI, nftName,nftDescription,nftTag, {from: user1});
+			expect(NFTCollection).to.be.ok;
+		});
+		it("should return 1 as first token ID.", async () => {
+            const nftTokenID = await masterInstance.createNFT.call(nftCollectionAddress, user1, tokenURI, nftName,nftDescription,nftTag, {from: user1});
+            expect(new BN(nftTokenID)).to.be.bignumber.equal(new BN(1));
+        });
+	});
+
+	describe("Testing setNewPrice function", () => {
+		beforeEach(async () => {
+			CYONTokenInstance = await CYONToken.new('10000000000000000000000000',{from:owner});
+			await CYONTokenInstance.transfer(CYONTokenInstance.address, '10000000000000000000000000');
+            masterInstance = await Master.new(CYONTokenInstance.address,{from:owner});
+            nftFactoryInstance = await Factory.new({from:owner});
+			nftCollection = await nftFactoryInstance.createNFTCollection(nftCollectionName,nftCollectionSymbol,{from: user1});
+			nftCollectionAddress = nftCollection.logs[0].args._collectionAddress;
+			await masterInstance.createNFT(nftCollectionAddress, user1, tokenURI, nftName,nftDescription,nftTag, {from: user1});
+		});
+		it("should revert when the collection address is set to zero.", async () => {
+            await expectRevert(masterInstance.setNewPrice(addressZero, 1, 10,{from: user1}), 'The collection address needs to be different from zero.');
+        });
+		it("should revert when a user different from the owner calls the function.", async () => {
+            await expectRevert(masterInstance.setNewPrice(nftCollectionAddress, 1, 10,{from: user2}), 'This NFT does not belong to the current message sender.');
+        });
+		it("should revert when the price is set to 0.", async () => {
+            await expectRevert(masterInstance.setNewPrice(nftCollectionAddress, 1, 0,{from: user1}), 'Please define a selling price higher than zero.');
+        });
+		it("should set the price.", async () => {
+			await masterInstance.setNewPrice(nftCollectionAddress, 1, 10,{from: user1});
+			expect(NFTCollection).to.be.ok;
+		});
+	});
+
+	describe("Testing buyNFT function", () => {
+		beforeEach(async () => {
+			CYONTokenInstance = await CYONToken.new('10000000000000000000000000',{from:owner});
+			await CYONTokenInstance.transfer(CYONTokenInstance.address, '10000000000000000000000000');
+            masterInstance = await Master.new(CYONTokenInstance.address,{from:owner});
+            nftFactoryInstance = await Factory.new({from:owner});
+			nftCollection = await nftFactoryInstance.createNFTCollection(nftCollectionName,nftCollectionSymbol,{from: user1});
+			nftCollectionAddress = nftCollection.logs[0].args._collectionAddress;
+			await masterInstance.createNFT(nftCollectionAddress, user1, tokenURI, nftName,nftDescription,nftTag, {from: user1});
+			await CYONTokenInstance.swapETHtoCYON({from: user1, value: 10});
+			await CYONTokenInstance.swapETHtoCYON({from: user2, value: 10});
+		});
+		it("should revert when the collection address is set to zero.", async () => {
+            await expectRevert(masterInstance.buyNFT(addressZero, 1, {from: user2}), 'The collection address needs to be different from zero.');
+        });
+		it("should revert when the NFT is not for sale.", async () => {
+            await expectRevert(masterInstance.buyNFT(nftCollectionAddress, 1, {from: user2}), 'This NFT is not for sale.');
+        });
+		it("should revert when Master contract did not get the allowance to transfer CYON token.", async () => {
+			await masterInstance.setNewPrice(nftCollectionAddress, 1, 10,{from: user1});
+            await expectRevert(masterInstance.buyNFT(nftCollectionAddress, 1, {from: user2}), 'CYON token allowance too low.');
+        });
+		it.skip("should revert when Master contract did not get the allowance to transfer CYON token.", async () => {
+			const price = new BN(10);
+			await masterInstance.setNewPrice(nftCollectionAddress, 1, price,{from: user1});
+            await CYONTokenInstance.approve(masterInstance.address,new BN(1000), {from: user1});
+			const oldBalance1 = await CYONTokenInstance.balanceOf(user1);
+			await masterInstance.buyNFT(nftCollectionAddress, 1, {from: user2})
+			const newBalance1 = await CYONTokenInstance.balanceOf(user1);
+			const newBalanceCalc = oldBalance1 + price;
+			
+			expect(newBalance1).to.be.equal(newBalanceCalc);
+        });
 	});
 });
