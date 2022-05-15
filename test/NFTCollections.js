7e885762784b632b9b1bae215ffde0cca68ac2a2
DeltaUpsilonDu <subscr1pt10n+g1t@myfr33dom.net>
2022-05-15 17:47:11 +0200

    Adding of the tests for the NFT collection.

--- test/NFTCollections.js
+++ test/NFTCollections.js
@@ -3,13 +3,12 @@ const {BN, expectRevert, expectEvent} = require('../node_modules/@openzeppelin/t
 const {expect} = require('chai');
 
 contract('NFTCollections', accounts => {
-     //Address used for test
+    //------------------------------------------------------------------------------------
+    // ----------------------------------Variables----------------------------------------
+    //------------------------------------------------------------------------------------
     const owner = accounts[0];
     const user1 = accounts[1];
     const user2 = accounts[2];
-    const user3 = accounts[3];
-    const user4 = accounts[4];
-    const addressZero = '0x0000000000000000000000000000000000000000';
     let NFTCollectionsInstance;
 
     const nftCollectionName = 'Collection1';
@@ -19,38 +18,64 @@ contract('NFTCollections', accounts => {
     const nftDescription = 'This is my first NFT';
     const nftTag = 'Collectible';
 
-    describe("### ** **", function () {
+    //------------------------------------------------------------------------------------
+    // ------------------------------------Tests------------------------------------------
+    //------------------------------------------------------------------------------------
+    describe("MintNFT function test", function () {
         beforeEach(async function () {
             NFTCollectionsInstance = await NFTCollections.new(nftCollectionName,nftCollectionSymbol,{from:owner});
-            await NFTCollectionsInstance.MintNFT(owner,owner,tokenURI,nftName,nftDescription,nftTag,user2,0,false,false,{ from: owner }); 
         });
-
-        it("Test...", async () => {
-            //await NFTCollectionsInstance.xxx({from:owner});
-            //const nftTokenID = await NFTCollectionsInstance.MintNFT.call(owner,owner,tokenURI,nftName,nftDescription,nftTag,user2,0,false,false,{ from: owner }); 
-            //console.log(nftTokenID.toString());
-
-            //OK  expect(await NFTCollectionsInstance.tokenURI(1,{from:owner})).to.equal(tokenURI);
-            //OK  expect(new BN(await NFTCollectionsInstance.balanceOf(owner,{ from: owner }))).to.be.bignumber.equal(new BN(1));
-            expect(await NFTCollectionsInstance.ownerOf(1,{from:owner})).to.equal(owner);
-
-            /*
-            const name = await NFTCollectionsInstance.name();
-            console.log(name.toString());
-
-            const balance = await NFTCollectionsInstance.balanceOf(owner,{ from: owner });
-            console.log(balance.toString());
-
-            //expect(name).to.equal(nftCollectionName);
-            expect(new BN(balance)).to.be.bignumber.equal(new BN(1));
-/*
-
-
-            const price = await NFTCollectionsInstance.getPrice(nftTokenID.toString(),{ from: owner });
-            console-log(price.toString());
-
-            //await expectRevert(NFTCollectionsInstance.xxx(owner, {from:owner}), "xx");*/
+        it("should return 1 as first token ID.", async () => {
+            const nftTokenID = await NFTCollectionsInstance.MintNFT.call(owner,owner,tokenURI,nftName,nftDescription,nftTag,user2,0,false,false,{ from: owner });
+            console.log(nftTokenID.toString());
+            expect(new BN(nftTokenID)).to.be.bignumber.equal(new BN(1));
         });
+        it("should return 2 as second token ID.", async () => {
+            await NFTCollectionsInstance.MintNFT(owner,owner,tokenURI,nftName,nftDescription,nftTag,user2,0,false,false,{ from: owner });
+            const nftTokenID = await NFTCollectionsInstance.MintNFT.call(owner,owner,tokenURI,nftName,nftDescription,nftTag,user2,0,false,false,{ from: owner });
+            console.log(nftTokenID.toString());
+            expect(new BN(nftTokenID)).to.be.bignumber.equal(new BN(2));
+        });
+        it("should mint the NFT to the correct owner.", async () => {
+            await NFTCollectionsInstance.MintNFT(user1,owner,tokenURI,nftName,nftDescription,nftTag,user2,0,false,false,{ from: owner });
+            expect(await NFTCollectionsInstance.ownerOf(1,{from:owner})).to.equal(user1);
+        });
+        it("should update the balance of the owner.", async () => {
+            await NFTCollectionsInstance.MintNFT(user1,owner,tokenURI,nftName,nftDescription,nftTag,user2,0,false,false,{ from: owner });
+            expect(new BN(await NFTCollectionsInstance.balanceOf(user1,{ from: owner }))).to.be.bignumber.equal(new BN(1));
+        });
+        it("should send the URI after the mint.", async () => {
+            await NFTCollectionsInstance.MintNFT(owner,owner,tokenURI,nftName,nftDescription,nftTag,user2,0,false,false,{ from: owner });
+            expect(await NFTCollectionsInstance.tokenURI(1,{from:owner})).to.equal(tokenURI);
+        });
+        it("should raise an event when a NFT is created.", async () => {
+            const findEvent = await NFTCollectionsInstance.MintNFT(owner,owner,tokenURI,nftName,nftDescription,nftTag,user2,0,false,false,{ from: owner });
+            console.log(findEvent);
+            expectEvent(findEvent,"NFTCreated" ,{_collectionName: nftCollectionName, _collectionAddress: NFTCollectionsInstance.address, _tokenID: new BN(1), _collectionData: {name: nftName, description: nftDescription, tag: nftTag, tokenAddress: user2, price: new BN(0), favorite: false, isAuctionable: false},_creator: owner});
+        }); 
+        
     });
-
+    describe("setPrice function test", function () {
+        before(async function () {
+            NFTCollectionsInstance = await NFTCollections.new(nftCollectionName,nftCollectionSymbol,{from:owner});
+            await NFTCollectionsInstance.MintNFT(owner,owner,tokenURI,nftName,nftDescription,nftTag,user2,0,false,false,{ from: owner });
+        });
+        it("should raise an event when the price is modified.", async () => {
+            const price = new BN(10);
+            const findEvent = await NFTCollectionsInstance.setPrice(1,price,{from:owner});
+            expectEvent(findEvent,"NewPriceSet" ,{_collectionName: nftCollectionName, _collectionAddress: NFTCollectionsInstance.address, _tokenID: new BN(1), _price: price});
+        }); 
+    });
+    describe("getPrice function test", function () {
+        before(async function () {
+            NFTCollectionsInstance = await NFTCollections.new(nftCollectionName,nftCollectionSymbol,{from:owner});
+            await NFTCollectionsInstance.MintNFT(owner,owner,tokenURI,nftName,nftDescription,nftTag,user2,0,false,false,{ from: owner });
+        });
+        it("should return the correct price (10).", async () => {
+            const price = new BN(10);
+            await NFTCollectionsInstance.setPrice(1,price,{from:owner});
+            const result = await NFTCollectionsInstance.getPrice.call(1,{from:owner})
+            expect(new BN(result)).to.be.bignumber.equal(new BN(price));
+        }); 
+    });
 });
